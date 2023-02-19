d43ed3d74ce9a8099b1dc98ff20f1c3e7989fd65
mdashif5@gmail.com <55871288+mdashifreza@users.noreply.github.com>
2023-02-19 18:37:30 +0530

    twoSum Using Map

--- twoSum.js
+++ twoSum.js
@@ -1,35 +1,39 @@
 //Time comple O(n^2);
-function twoSum(arr,find){
-    for(let i=0; i<arr.length-1; i++)
-    {
-        for(let j=0; j<arr.length; j++)
-        {
-            if(arr[i]+ arr[j] == find)
-            {
-                return [i,j];
-            }
-        }
-    }
-}
-let result = twoSum([2,4,6,8,10],10);
-console.log(result);
+// function twoSum(arr,find){
+//     for(let i=0; i<arr.length-1; i++)
+//     {
+//         for(let j=0; j<arr.length; j++)
+//         {
+//             if(arr[i]+ arr[j] == find)
+//             {
+//                 return [i,j];
+//             }
+//         }
+//     }
+// }
+// let result = twoSum([2,4,6,8,10],10);
+// console.log(result);
 //Time comp O(n)
+console.log('--------------------------')
 let arr = [2,4,6,8,10];
-let target = 10;
-let twoSumMap = function(arr,target){
+let target = 12;
+function twoSumMap(arr,target){
+    let map = new Map();
     for(let i=0; i<arr.length; i++)
     {
-        let num1 = arr[i];
-        let num2 = target - num1;
-        if(map.has(num2))
+        let valueArr = arr[i];
+        let indexInMap = target - valueArr;
+        // map.set(i,num1); i=> key and value => value 
+        if(map.has(indexInMap))
         {
-            return [map.get(num2),i]
+            return [i,map.get(indexInMap)];
         }
         else
         {
-            return map.set(num1,i);
+            map.set(i,valueArr);
         }
     }
+    console.log(map)
 }
 let resu = twoSumMap(arr,target);
-let map = new Map();
+console.log(resu);
