51e4c5e0fee579d2de6359e1d54faaa928f8a634
mdashif5@gmail.com <55871288+mdashifreza@users.noreply.github.com>
2023-03-01 02:42:44 +0530

    week-1-p5

--- twoSum.js
+++ twoSum.js
@@ -1,39 +1,16 @@
-//Time comple O(n^2);
-// function twoSum(arr,find){
-//     for(let i=0; i<arr.length-1; i++)
-//     {
-//         for(let j=0; j<arr.length; j++)
-//         {
-//             if(arr[i]+ arr[j] == find)
-//             {
-//                 return [i,j];
-//             }
-//         }
-//     }
-// }
-// let result = twoSum([2,4,6,8,10],10);
-// console.log(result);
-//Time comp O(n)
-console.log('--------------------------')
-let arr = [2,4,6,8,10];
-let target = 12;
-function twoSumMap(arr,target){
-    let map = new Map();
-    for(let i=0; i<arr.length; i++)
+//q1 simple return the pair present
+function twoSum(arr,target){
+    const hashmap = {};
+    let len = arr.length;
+    for(let i=0; i<len; i++)
     {
-        let valueArr = arr[i];
-        let indexInMap = target - valueArr;
-        // map.set(i,num1); i=> key and value => value 
-        if(map.has(indexInMap))
+        if( hashmap[target-arr[i]] !== undefined )
         {
-            return [i,map.get(indexInMap)];
+            return [hashmap[target - arr[i]],i];
         }
-        else
-        {
-            map.set(i,valueArr);
-        }
+        hashmap[arr[i]] = i;
     }
-    console.log(map)
+return [-1,-1];
 }
-let resu = twoSumMap(arr,target);
-console.log(resu);
+let result = twoSum([0,1,2,4,6,3],5);
+console.log(result);
