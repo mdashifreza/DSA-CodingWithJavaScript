5cc44b328651c0d12b8146e521acde35cdfe8390
mdashif5@gmail.com <55871288+mdashifreza@users.noreply.github.com>
2023-03-07 13:58:58 +0530

    week-2-p17

---
+++ isomorphicString.js
@@ -0,0 +1,31 @@
+var isomorphicString = function(s,t){
+    const hashmapST = {};
+    const hashmapTS = {};
+    if(s.length != t.length)
+    {
+        return ;
+    }
+    for(let i=0; i<s.length; i++)
+    {
+        let chS = s[i];
+        let chT = t[i];
+        if(!hashmapST[chS])
+        {
+            hashmapST[chS] = chT;
+        } 
+        if(!hashmapTS[chT])
+        {
+            hashmapTS[chT] = chS;
+        }
+        if(hashmapST[chS] !== chT || hashmapTS[chT] !== chS)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isomorphicString('foo','adj'));
+let a = 'a';
+let b = 'a';
+console.log( 0 != a);
+console.log( 0 != b);
