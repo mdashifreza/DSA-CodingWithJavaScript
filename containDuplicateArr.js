8e9fb3ac9f2e6e0aba2e1d74b7ff252db001a236
mdashif5@gmail.com <55871288+mdashifreza@users.noreply.github.com>
2023-03-09 00:47:41 +0530

    week-3

--- containDuplicateArr.js
+++ containDuplicateArr.js
@@ -1,3 +1,4 @@
+//in O(n^2) 
 var containDuplicateArr = function(nums,k){
     let len = nums.length;
     for( let i=0; i<len-1; i++)
@@ -12,4 +13,18 @@ var containDuplicateArr = function(nums,k){
     }
     return false;
 }
-console.log(containDuplicateArr([1,2,3,1,2,3]));
+console.log(containDuplicateArr([1,2,3,1,2,3],2));console.log('--------------');
+//in O(log(n))
+var containDuplicateArr2 = function(nums,k){
+    let hashMapSeen = {};
+    for (let i = 0; i < nums.length; i++)
+    {
+        if (nums[i] in hashMapSeen && i - hashMapSeen[nums[i]] <= k) 
+        {
+            return true;
+        }
+        hashMapSeen[nums[i]] = i;
+    }
+return false;
+}
+console.log(containDuplicateArr2([1,2,3,1,2,3],2))
