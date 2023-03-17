9260c503cb5649dbc9073a6d4e846459a019ae60
mdashif5@gmail.com <55871288+mdashifreza@users.noreply.github.com>
2023-03-17 16:50:23 +0530

    week-4-all solution

--- thirdMaximum.js
+++ thirdMaximum.js
@@ -1,4 +1,22 @@
-
+//nlogn
+var thirdMaximum_1 =  function(nums){
+    const setOfnums = new Set(nums);
+    const newNums = [...setOfnums];
+    newNums.sort((a,b)=>b-a); //3,2,1
+    let firstMax = -Infinity;
+    let thirdMax = 2; //2
+    firstMax = Math.max(firstMax,...newNums);
+    let len = newNums.length; //3
+    if(len === 2){
+        return firstMax;
+    }
+    for(let i=0; i<len; i++){// i = 0,1,2
+        if(i === thirdMax){ 
+            return newNums[thirdMax]; 
+        }
+    }
+}
+console.log(thirdMaximum_1([1,2,3,1,2,3,3]));
 // [1,2,3,1,2,3,4,5,6,7,6,5]
 //O(n)
 var thirdMaximum_2 = function(nums){
